d7095323e3502ead7353b623bb1c07b87f22e09c
smy2363 <smy2363@naver.com>
2024-09-27 09:05:07 +0900

    0926

--- cLanguage/cLanguage/struct2.c
+++ cLanguage/cLanguage/struct2.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 // struct2.c
 
 typedef struct music {
-	char singer[20]; //가수명
-	int release; // 발매년도
-	float playtime; //재생시간
+	char singer[20]; // 가수명
+	int release;  // 발매년도
+	float playtime; // 재생시간
 
 }music;
 
@@ -15,50 +14,48 @@ music* input();
 void print(music**);
 
 void main() {
-	//music* m1 = (music*)malloc(sizeof(music*));
-	//m1->release = 2019;
-	//m1->playtime = 4.41;
-	//strcpy_s(m1->singer, 20,"폴킴");
+	/*music* m1 = (music*)malloc( sizeof(music) );
+	m1->release = 2019;
+	m1->playtime = 4.41;
+	strcpy_s(m1->singer, 20, "폴킴");*/
 
-	int i = 1;
+
 	music** data = NULL;
 
 	for (int i = 1; i <= 5; i++) {
 		if (data == NULL)
-			data = (music**)malloc(i * sizeof(music*));
+			data = (music**)malloc(sizeof(music*));
 		else
 			data = (music**)realloc(data, i * sizeof(music*));
 
 		data[i - 1] = input();
-
 	}
 
-	print(data); 
+	print(data);
 
 	//할당 메모리 해제
 	for (int i = 0; i < 5; i++) {
 		free(data[i]);
 	}
 	free(data);
+
 }
 
 music* input() {
 	music* newData = (music*)malloc(sizeof(music));
-	printf("가수 :");
+	printf("가수 : ");
 	gets_s(newData->singer, 20);
 	printf("발매년도 : ");
 	scanf_s("%d", &newData->release);
-	printf("재생시간 : ");
+	printf("재생 시간 : ");
 	scanf_s("%f", &newData->playtime);
 	getchar();
-	
 	return newData;
-
 }
 
 void print(music** p) {
 	for (int i = 0; i < 5; i++) {
-		printf("%s (%d년) - %.2f분 \n", p[i]->singer, p[i]->release, p[i]->playtime);
+		printf("%s (%d년) - %.2f \n",
+			p[i]->singer, p[i]->release, p[i]->playtime);
 	}
-
 }
